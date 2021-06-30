ffbd69757ecb0daaaec10fe6bbb7cec3198a9bc2
wangfong232 <86735806+wangfong232@users.noreply.github.com>
2021-07-01 00:05:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'crush à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao mình cứ đi nhầm đường
+    Lạc vào tim cậu lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Phải chăng mình đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng mình đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Mình thích cậu <33',
+    btnNo: 'Không, tụi mình chỉ là bạn :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại thích mình <3',
+    btnReply: 'Gửi cho mình <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'iu em quá :3.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'mình thích cậu, cậu có thích mình khum 😘😘',
+    messDesc: 'iu cậu quá :3.',
+    btnAccept: 'yêu nhau luôn đi <3',
     messLink: 'http://fb.com/chancewangfong' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
